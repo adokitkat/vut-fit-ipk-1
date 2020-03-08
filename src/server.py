1d5d3794aa7b361c0c3f1f00402370a33324bbc4
Adam Múdry <mudry.ado@gmail.com>
2020-03-08 22:24:18 +0100

    changed handling of bad requests

--- src/server.py
+++ src/server.py
@@ -2,15 +2,36 @@ import http.server, socket, socketserver, sys, re, cgi
 
 class Handler (http.server.BaseHTTPRequestHandler):
 
-  verbose_response = {200: "OK.", 400: "Bad Request.", 404: "Not Found.", 405: "Method Not Allowed.", 500: "Internal Server Error."}
+  verbose_response = {200: "OK", 400: "Bad Request.", 404: "Not Found.", 405: "Method Not Allowed.", 500: "Internal Server Error."}
+
+  def parse_request(self):
+    """
+    Alters behaviour of identical named parent's method to accept only GET and POST requests
+    """
+    super().parse_request()
+    if self.command != 'GET' and self.command != 'POST':
+      self.response(405, self.verbose_response[405])
+      return
+    elif self.command == 'GET':
+      self.GET()
+    elif self.command == 'POST':
+      self.POST()
+    self.wfile.flush()
+  
 
   def response(self, n, msg=None):
+    """
+    Quick response method
+    """
     self.protocol_version = 'HTTP/1.1'
     self.send_response(n, msg)
-    self.send_header("Content-type", "text/html")
+    self.send_header("Content-type", "text/plain")
     self.end_headers()
 
   def validate(self, line, method):
+    """
+    Validates request
+    """
     if method == 'GET':
       re_type = r"(?<=type=)(A|PTR)(?=&|$)"
     elif method == 'POST':
@@ -49,7 +70,10 @@ class Handler (http.server.BaseHTTPRequestHandler):
     else:
       return (200, name_or_ip.group(0), response_type.group(0), resolved_name_or_ip)
 
-  def do_GET(self):
+  def GET(self):
+    """
+    Answers GET requests
+    """
     if re.search(r"^/resolve", self.path) == None:
       self.response(400, self.verbose_response[400])
       return
@@ -59,7 +83,10 @@ class Handler (http.server.BaseHTTPRequestHandler):
     if code == 200:
       self.wfile.write((name_or_ip + ':' + response_type + '=' + resolved_name_or_ip).encode('utf-8'))
 
-  def do_POST(self):
+  def POST(self):
+    """
+    Answers POST requests
+    """
     if self.path != "/dns-query":
       self.response(400, self.verbose_response[400])
       return
@@ -72,42 +99,25 @@ class Handler (http.server.BaseHTTPRequestHandler):
       code, name_or_ip, response_type, resolved_name_or_ip = self.validate(line, 'POST')
       codes.append(code)
       if code == 200:
-        output += (name_or_ip + ':' + response_type + '=' + resolved_name_or_ip + '\r\n')
+        output += (name_or_ip + ':' + response_type + '=' + resolved_name_or_ip + '\n')
 
     if 200 in codes:
       self.response(200, self.verbose_response[200])
       self.wfile.write(output.rstrip().encode('utf-8'))
+    elif 400 in codes:
+      self.response(400, self.verbose_response[400])
     else:
       self.response(code, self.verbose_response[code])
 
-  def do_HEAD(self):
-    self.response(405, self.verbose_response[405])
-
-  def do_PUT(self):
-    self.response(405, self.verbose_response[405])
-
-  def do_DELETE(self):
-    self.response(405, self.verbose_response[405])
-  
-  def do_CONNECT(self):
-    self.response(405, self.verbose_response[405])
-
-  def do_OPTIONS(self):
-    self.response(405, self.verbose_response[405])
-
-  def do_TRACE(self):
-    self.response(405, self.verbose_response[405])
-
-  def do_PATCH(self):
-    self.response(405, self.verbose_response[405])
-
 if __name__ == "__main__":
 
+  # Sets port
   if len(sys.argv) > 1 and sys.argv[1].isdigit() and int(sys.argv[1]) >= 0 and int(sys.argv[1]) <= 65535:
     PORT = int(sys.argv[1])
   else:
     PORT = 5353
 
+  # Server init
   with socketserver.TCPServer(("", PORT), Handler) as server:
       try:
         server.serve_forever()
